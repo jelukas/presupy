eb6fe2b554a9c63b8daded97bf77350053b3832d
Jesús Lucas <jelukas89@gmail.com>
2012-09-10 00:29:25 +0200

    grapelly created

--- presupy/settings.py
+++ presupy/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'presu',
     # Uncomment the next line to enable the admin:
+    'grappelli',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
